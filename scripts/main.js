e95cb31110b5f48cd50f027beac6c86311fde72d
bMitko <mitko3@yahoo.com>
2025-04-29 00:15:57 +0200

    NavBar(global) created

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+const html = {
+    navBar: document.getElementById("navBar"),
+    logoPic: document.getElementById("logoPic"),
+    logoPicNeg: document.getElementById("logoPicNeg"),
+
+    menuToggle: document.getElementById("menuToggle"),
+    menu: document.getElementById("menuDiv"),
+
+    toggleBtn: document.getElementById('settingsToggle'),
+    dropdown: document.getElementById('settingsDropdown')
+};
+
+window.addEventListener("scroll", () => {
+    if(window.scrollY > 0){
+        html.navBar.classList.add("scrolledNav");
+    }
+    else {
+        html.navBar.classList.remove("scrolledNav");
+    }
+});
+
+html.menuToggle.addEventListener("click", () => {
+    html.menu.classList.toggle("show");
+});
+
+html.toggleBtn.addEventListener("click", () => {
+    html.dropdown.classList.toggle("show");
+});
+
+window.addEventListener("click", (e) => {
+    if (!html.dropdown.contains(e.target) && !html.toggleBtn.contains(e.target)) {
+      html.dropdown.classList.remove("show");
+    }
+  });
