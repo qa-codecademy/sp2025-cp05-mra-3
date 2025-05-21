712669a7300a1b41c1f38a1a54de7930c89102fd
bMitko <mitko3@yahoo.com>
2025-05-21 12:50:07 +0200

    merged and fixed

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,16 @@
 const html = {
     navBar: document.getElementById("navBar"),
-    logoPic: document.getElementById("logoPic"),
 
     menuToggle: document.getElementById("menuToggle"),
     menu: document.getElementById("menuDiv"),
 
-    toggleBtn: document.getElementById('settingsToggle'),
-    dropdown: document.getElementById('settingsDropdown')
+    dropdown: document.getElementById('languageDropdown'),
+    toggle: document.getElementById('dropdownToggle')
+
 };
 
 window.addEventListener("scroll", () => {
-    if(window.scrollY > 0){
+    if (window.scrollY > 0) {
         html.navBar.classList.add("scrolledNav");
     }
     else {
@@ -22,12 +22,24 @@ html.menuToggle.addEventListener("click", () => {
     html.menu.classList.toggle("show");
 });
 
-html.toggleBtn.addEventListener("click", () => {
-    html.dropdown.classList.toggle("show");
+const menuLinks = html.dropdown.querySelectorAll('.dropdown-menu a');
+const toggleImg = html.toggle.querySelector('img');
+
+html.toggle.addEventListener('click', (e) => {
+    e.stopPropagation();
+    html.dropdown.classList.toggle('open');
 });
 
-window.addEventListener("click", (e) => {
-    if (!html.dropdown.contains(e.target) && !html.toggleBtn.contains(e.target)) {
-      html.dropdown.classList.remove("show");
-    }
-  });
+menuLinks.forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const selectedFlag = link.getAttribute('data-flag');
+
+        toggleImg.src = selectedFlag;
+        html.dropdown.classList.remove('open');
+    });
+});
+
+document.addEventListener('click', () => {
+    html.dropdown.classList.remove('open');
+});
