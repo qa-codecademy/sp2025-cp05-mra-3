40a0f90eca7278a344a0c3f1961153a7cb77ee57
Zoran Nakov <nakovzoran99@gmail.com>
2025-06-18 21:57:23 +0200

    merget main into this branch

---
+++ scripts/main.js
@@ -0,0 +1,45 @@
+const html = {
+    navBar: document.getElementById("navBar"),
+
+    menuToggle: document.getElementById("menuToggle"),
+    menu: document.getElementById("menuDiv"),
+
+    dropdown: document.getElementById('languageDropdown'),
+    toggle: document.getElementById('dropdownToggle')
+
+};
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 0) {
+        html.navBar.classList.add("scrolledNav");
+    }
+    else {
+        html.navBar.classList.remove("scrolledNav");
+    }
+});
+
+html.menuToggle.addEventListener("click", () => {
+    html.menu.classList.toggle("show");
+});
+
+const menuLinks = html.dropdown.querySelectorAll('.dropdown-menu a');
+const toggleImg = html.toggle.querySelector('img');
+
+html.toggle.addEventListener('click', (e) => {
+    e.stopPropagation();
+    html.dropdown.classList.toggle('open');
+});
+
+menuLinks.forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const selectedFlag = link.getAttribute('data-flag');
+
+        toggleImg.src = selectedFlag;
+        html.dropdown.classList.remove('open');
+    });
+});
+
+document.addEventListener('click', () => {
+    html.dropdown.classList.remove('open');
+});
