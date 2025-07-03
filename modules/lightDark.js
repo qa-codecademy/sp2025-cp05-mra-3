24049fa70f22ea4c7bc9ef935a3c2103b90cf912
David Krstevski <krstevskidavid998@gmail.com>
2025-07-03 20:49:31 +0200

    Update

--- modules/lightDark.js
+++ modules/lightDark.js
@@ -155,6 +155,88 @@ window.onload = function () {
   .dark-mode #submitToggleNewReview {
       background-color: rgb(60, 120, 200);
   }
+   .dark-mode #login-form{
+    box-shadow: 0 0 10px rgb(118, 167, 231);
+      background-color: rgb(35, 35, 35);
+      border-color: rgb(70, 70, 70);
+      color: rgb(200, 230, 255);
+   }
+    .dark-mode #login-form input,
+    .dark-mode #login-form textarea {
+      background-color: rgb(45, 45, 45);
+      border: 1px solid rgb(90, 90, 90);
+      color: rgb(200, 230, 255);
+     }
+    .dark-mode #login-form h2{
+    color: rgb(200, 230, 255);
+    }
+    .dark-mode #login-form label{
+    color: rgb(200, 230, 255);
+    }
+    .dark-mode #userTableWrapper,
+    .dark-mode #cardTableWrapper,
+    .dark-mode #signup-form-wrapper,
+    .dark-mode #emailTableWrapper,
+    .dark-mode #contentTableWrapper,
+    .dark-mode #card-form-wrapper,
+    .dark-mode #reviewTableWrapper {
+     box-shadow: 0 0 10px rgb(118, 167, 231);
+      background-color: rgb(35, 35, 35);
+      border-color: rgb(70, 70, 70);
+      color: rgb(200, 230, 255);
+    }
+    .dark-mode .content-entry,
+    .dark-mode .user-entry,
+    .dark-mode .email-entry,
+    .dark-mode .review-entry,
+    .dark-mode .card-entry{
+    box-shadow: 0 0 10px rgb(118, 167, 231);
+     background-color: rgb(35, 35, 35);
+    border-color: rgb(70, 70, 70);
+    }
+    .dark-mode #leftVerticalMenu{
+     box-shadow: 0 0 10px rgb(118, 167, 231);
+     background-color: rgb(35, 35, 35);
+    border-color: rgb(70, 70, 70);
+    }
+    .dark-mode #leftVerticalMenu button{
+          background-color: rgb(60, 120, 200);
+      color: white;
+    }
+    .dark-mode #userSignupForm input{
+      background-color: rgb(45, 45, 45);
+      color: rgb(200, 230, 255);
+      border: 1px solid rgb(90, 90, 90);
+      }
+    .dark-mode #signup-form-wrapper h2,
+    .dark-mode #signup-form-wrapper label{
+    color: rgb(200, 230, 255);
+      }
+    .dark-mode .user-entry button,
+    .dark-mode  .review-entry button,
+    .dark-mode .content-entry button,
+    .dark-mode .card-entry button{
+     background-color: rgb(60, 120, 200);
+      color: white;
+    }
+    .dark-mode #card-form-wrapper input,
+    .dark-mode #card-form-wrapper textarea{
+      background-color: rgb(45, 45, 45);
+      color: rgb(200, 230, 255);
+      border: 1px solid rgb(90, 90, 90);
+      }
+      .dark-mode #card-form-wrapper h2,
+      .dark-mode #card-form-wrapper textarea::placeholder{
+      color: rgb(200, 230, 255);
+      }
+        .dark-mode #card-form-wrapper h2,
+      .dark-mode #card-form-wrapper input::placeholder,
+      .dark-mode #card-form-wrapper label{
+      color: rgb(200, 230, 255);
+      }
+      .dark-mode #leftVerticalMenu #logOutButton {
+      background-color: red;
+      }
 `;
 
   const styleTag = document.createElement("style");
